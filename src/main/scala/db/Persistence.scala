aae7441f56060997743c974be313b5b39ffe7252
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-18 00:02:18 -0600

    IO handled in a more functional way

--- src/main/scala/db/Persistence.scala
+++ src/main/scala/db/Persistence.scala
@@ -1,23 +1,45 @@
 package org.aranadedoros
 package db
 
-import auth.Security.{DecryptedPassword, EncryptedPassword, User}
+import auth.Security.KeyProvider.given_SecretKey
+import auth.Security.{Crypto, DecryptedPassword, EncryptedPassword, User}
+import db.Persistence.Database
 
 import java.nio.file.{Files, Paths, StandardOpenOption}
 import scala.collection.immutable.HashMap
 
 object FileUtils:
-  def writeBytes(path: String, data: Array[Byte]): Unit =
-    Files.write(
-      Paths.get(path),
-      data,
-      StandardOpenOption.CREATE,
-      StandardOpenOption.APPEND,
-      StandardOpenOption.WRITE
-    )
 
-  def readBytes(path: String): Array[Byte] =
-    Files.readAllBytes(Paths.get(path))
+  def init(user: String, password: String): Either[Throwable, Database] =
+    val usr          = User(user, password)
+    val db           = Database("db", usr)
+    val usrEncrypted = Crypto.encrypt(s"$usr".getBytes("UTF-8"))
+    val contentEnc   = Crypto.encrypt("^".getBytes("UTF-8"))
+    for
+      _ <- writeBytes("usr.enc", usrEncrypted)
+      _ <- writeBytes("db.enc", contentEnc)
+    yield db
+
+  def writeBytes(path: String, data: Array[Byte]): Either[Throwable, Unit] =
+    try
+      Files.write(
+        Paths.get(path),
+        data,
+        StandardOpenOption.CREATE,
+        StandardOpenOption.TRUNCATE_EXISTING
+      )
+      Right(())
+    catch
+      case e: Throwable =>
+        Left(e)
+
+  def readBytes(path: String): Either[Throwable, Array[Byte]] =
+    try
+      val bytes = Files.readAllBytes(Paths.get(path))
+      Right(bytes)
+    catch
+      case e: Throwable =>
+        Left(e)
 
 object Persistence {
   opaque type EntryDictionary = HashMap[String, Entry]
@@ -26,7 +48,7 @@ object Persistence {
     def password = DecryptedPassword(EncryptedPassword(passw))
   }
 
-  case class Database(name: String, usr: User, entries: EntryDictionary = HashMap.empty):
+  case class Database(name: String, user: User, entries: EntryDictionary = HashMap.empty):
 
     def +(entry: Entry): Database =
       copy(entries = entries + (entry.title -> entry))
