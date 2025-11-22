63fba309d6df69816320423fe164708d4efd36ff
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-22 14:05:07 -0600

    cleaner imports and new DatabaseManager to load or creation of db

--- src/main/scala/persistence/Persistence.scala
+++ src/main/scala/persistence/Persistence.scala
@@ -1,11 +1,13 @@
 package org.aranadedoros
 package persistence
 
-import auth.Security.{MasterHashStorage, MasterPasswordService}
+import auth.Security.{MasterHashStorage, MasterPasswordService, PasswordManager}
 import model.Model.Database
 import serialization.{BinarySerialization, JsonSerialization}
+import java.io.File
+import scala.io.StdIn.readLine
 
-object Persistence {
+object Persistence:
   object FileUtils:
     def init(user: String, password: String): Either[Throwable, Array[Byte]] =
       // write for the 1st time
@@ -17,4 +19,22 @@ object Persistence {
         serialized <- JsonSerialization.serialize(db)
         _          <- BinarySerialization.writeBytes(serialized)
       yield serialized
-}
+
+  object DatabaseManager:
+    def loadOrCreate(password: String): Either[Throwable, Database] =
+      val dbFile = new File("db.enc")
+      if !dbFile.exists() then
+        println("Database does not exist. Creating...")
+        println("Enter username:")
+        val usr = readLine()
+        FileUtils.init(usr, password).flatMap(_ => JsonSerialization.readDatabase())
+      else
+        for
+          hash <- MasterHashStorage.loadHash()
+          mngr = new PasswordManager(hash)
+          valid <- mngr.validate(password) {
+            password.trim.nonEmpty && password.trim.length >= 6
+          }.left.map(msg => new Exception(msg))
+          db <- if valid then JsonSerialization.readDatabase()
+          else Left(new Exception("Invalid master password"))
+        yield db
