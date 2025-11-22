3d84f0f2e869a0f13aa17aa5d60cfecc24e7ffa9
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-22 13:27:57 -0600

    case reorder

--- src/main/scala/parsing/Parsers.scala
+++ src/main/scala/parsing/Parsers.scala
@@ -5,7 +5,6 @@ import model.Model.{EntryKey, Flag, Site}
 
 object Parsers {
   object FlagParser:
-
     def parse(args: List[String]): Either[String, Flag] =
       args match
         case "--add" :: rawSite :: rawKey :: Nil =>
@@ -18,15 +17,14 @@ object Parsers {
             site <- Site.fromRaw(rawSite)
             key  <- EntryKey.fromRaw(rawKey)
           yield Flag.Delete(site, key)
-        case "--list" :: Nil =>
-          Right(Flag.ListAll)
         case "--search" :: rawKey :: Nil =>
           EntryKey.fromRaw(rawKey).map(Flag.Search.apply)
+        case "--list" :: Nil =>
+          Right(Flag.ListAll)
         case "--init" :: Nil =>
           Right(Flag.Init)
         case "--help" :: Nil | Nil =>
           Right(Flag.Help)
-
         case other =>
           Left(s"Unknown command: ${other.mkString(" ")}")
 
